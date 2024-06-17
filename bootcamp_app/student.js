c5e6b08e1eaa6472a6a6ff61c3f7a702af7ccf13
SorrenJ <soalexjao@gmail.com>
2024-06-16 19:14:19 -0700

    paramatized student and teacher queries in js files

--- bootcamp_app/student.js
+++ bootcamp_app/student.js
@@ -6,18 +6,22 @@ const pool = new Pool({
   host: "localhost",
   database: "bootcampx",
 });
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
 
-
-pool
-  .query(
-    `
+queryText =`    
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`
-  )
+WHERE cohorts.name LIKE $1 
+LIMIT $2;
+`;
+
+
+const queryParams = [`%${cohortName}%`, limit];
+
+pool
+.query(queryText, queryParams)
   .then((res) => {
     res.rows.forEach((user) => {
       console.log(
